361d8791d47d3b83aab3faaf7cfeadc9b02948e1
--replace-all <r.carolinjeba@outlook.com>
2021-02-09 22:12:23 +0530

    Adding UI components UserItem, Header and Navigation

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-    return <h1>User works!</h1>
+    const USERS = [{
+        id: 'u1',
+        image: 'https://www.shareicon.net/data/2016/09/01/822727_user_512x512.png',
+        name: 'Carol',
+        places: 3
+    }];
+    return <UsersList items={USERS} />
 }
 
 export default Users;
